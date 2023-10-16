c94dc54d7ff31fd41e1fc7c0f963497420724706
Himanshu <kumarhimanshu2219@gmail.com>
2023-10-16 12:24:47 +0530

    login functionality added

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,36 @@
+import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 import { Button, CustomTitle } from "./";
 import { Input } from "./Input";
 import { login } from "../svgs";
+import env_config from "../config/env_config";
 
 const Login = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const navigate = useNavigate();
+  const API_ENDPOINT = env_config.VITE_API_ENDPOINTS;
+
+  const handleSubmit = (event) => event.preventDefault();
+  const userLogin = async () => {
+    try {
+      const res = await axios.post(`${API_ENDPOINT}/api/v1/auth/login`, {
+        email,
+        password,
+      });
+      if (res.data.success) {
+        alert(res.data.message);
+        navigate("/home");
+      } else {
+        alert(res.data.message);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <div className="flex justify-center">
@@ -10,22 +38,27 @@ const Login = () => {
       </div>
       <div className="flex justify-center items-center  ">
         <img src={login} alt="" className="w-[30rem]" />
-        <form>
+        <form onSubmit={handleSubmit}>
           <Input
             htmlFor="email"
             label={"email*"}
             type={"email"}
+            value={email}
+            handleChange={(event) => setEmail(event.target.value)}
             placeholder={"marvinAllen@gmail.com"}
           />
           <Input
             htmlFor="password"
             label={"password*"}
             type={"password"}
+            value={password}
+            handleChange={(event) => setPassword(event.target.value)}
             placeholder={"password"}
           />
 
           <Button
             title="LOGIN"
+            handleClick={userLogin}
             className="font-semibold bg-color_secondary text-color_white w-full mt-5 hover:scale-105 duration-100 transition-transform"
           />
         </form>
