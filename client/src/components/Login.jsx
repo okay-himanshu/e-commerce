7508719dd4e28242ac4d0bfba8878eda89fa7a76
Himanshu <kumarhimanshu2219@gmail.com>
2023-10-22 14:52:00 +0530

    login re-designed

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 import { Button, CustomTitle, Input } from "./";
 import { login } from "../svgs";
@@ -47,46 +47,108 @@ const Login = () => {
 
   return (
     <>
-      <div className="flex justify-center">
-        <CustomTitle title={"LOGIN"} />
-      </div>
-      <div className="flex justify-center items-center  ">
-        <img src={login} alt="" className="w-[30rem]" />
-        <form onSubmit={handleSubmit}>
-          <Input
-            htmlFor="email"
-            label={"email*"}
-            type={"email"}
-            value={email}
-            handleChange={(event) => setEmail(event.target.value)}
-            placeholder={"marvinAllen@gmail.com"}
-          />
-          <Input
-            htmlFor="password"
-            label={"password*"}
-            type={"password"}
-            value={password}
-            handleChange={(event) => setPassword(event.target.value)}
-            placeholder={"password"}
-          />
-          <Button
-            title="LOGIN"
-            handleClick={userLogin}
-            className="font-semibold bg-color_secondary text-color_white w-full mt-5 hover:scale-105 duration-100 transition-transform"
-          />
-          <div className="flex justify-center mt-3 text-sm text-color_primary ">
-            <Link to="/forget-password">Forgot password ?</Link>
-          </div>
+      <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 ">
+        <div className="mx-auto max-w-lg">
+          <form
+            onSubmit={handleSubmit}
+            className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
+          >
+            <p className="text-center text-lg font-medium">
+              Login to your account
+            </p>
 
-          <div className="border flex justify-center mt-5 p-2  ">
-            <div to="/forget-password">
-              Don't have an account?{" "}
-              <Link to="/signup" className="text-color_primary">
-                Signup
-              </Link>
+            <div>
+              <label htmlFor="email" className="sr-only">
+                Email
+              </label>
+
+              <div className="relative">
+                <input
+                  onChange={(event) => setEmail(event.target.value)}
+                  type="email"
+                  className="w-full border rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm focus:outline-green-600"
+                  placeholder="Enter email"
+                />
+
+                <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4 text-gray-400"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                    />
+                  </svg>
+                </span>
+              </div>
             </div>
-          </div>
-        </form>
+
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+
+              <div className="relative">
+                <input
+                  onChange={(event) => setPassword(event.target.value)}
+                  type="password"
+                  className="mb-2 w-full rounded-lg border border-gray-200 p-4 pe-12 text-sm shadow-sm focus:outline-green-600"
+                  placeholder="Enter password"
+                />
+
+                <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4 text-gray-400"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                    />
+                  </svg>
+                </span>
+                <NavLink
+                  to="/forget-password"
+                  className="text-sm text-gray-500 mt-2 ml-2"
+                >
+                  forget password ?
+                </NavLink>
+              </div>
+            </div>
+
+            <button
+              onClick={userLogin}
+              type="submit"
+              className="block w-full rounded-lg bg-green-600 hover:bg-green-700 duration-75 px-5 py-3 text-sm font-medium text-white"
+            >
+              Sign in
+            </button>
+
+            <p className="text-center text-sm text-gray-500">
+              No account?{" "}
+              <NavLink className="underline" to="/signup">
+                Sign up
+              </NavLink>
+            </p>
+          </form>
+        </div>
       </div>
     </>
   );
