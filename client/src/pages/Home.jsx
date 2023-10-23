0f59748f13047b065b72a0938b4478a7d360a30b
Himanshu <kumarhimanshu2219@gmail.com>
2023-10-23 14:24:22 +0530

    home page re-designed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 import { Button, Hero, SignUp, Prices } from "../components/index";
 import { hero1, hero2, hero3 } from "../images/index";
@@ -129,6 +129,7 @@ function Home() {
   const addToCart = (product) => {
     if (auth?.token) {
       setCart([...cart, product]);
+
       localStorage.setItem("cart", JSON.stringify([...cart, product]));
       alert("product added to cart");
     } else {
@@ -141,87 +142,156 @@ function Home() {
     window.location.reload();
   };
 
+  console.log(products);
   return (
     <>
       <Hero />
+      <main className="flex justify-center mx-5">
+        <div className="flex justify-between">
+          <div className="hidden sm:block ">
+            <div className="border">
+              <h1 className="py-4">FILTER'S BY CATEGORY</h1>
+              {categories.map((category) => (
+                <ul
+                  className="w-48  border-t flex items-center gap-2 border-gray-200 p-1 "
+                  key={category?._id}
+                >
+                  <div>
+                    <input
+                      type="checkbox"
+                      className=" w-5 rounded border-gray-300"
+                      onChange={(event) =>
+                        handleChecked(event.target.checked, category._id)
+                      }
+                    />
+                  </div>
 
-      <div className="flex">
-        <div className="w-80">
-          <h1>Filters By Categories</h1>
-          {categories.map((category) => (
-            <>
-              <div key={category._id}></div>
-              <div>
-                <h1>{category.name}</h1>
-                <input
-                  type="checkbox"
-                  onChange={(event) =>
-                    handleChecked(event.target.checked, category._id)
-                  }
-                />
-              </div>
-            </>
-          ))}
-          <h1>Filters By Price</h1>
-          {Prices.map((price) => (
-            <div key={price._id}>
-              <input
-                type="radio"
-                value={price.array}
-                checked={selectedPrice === price.array}
-                onChange={() => handleRadioChange(price.array)}
-              />
-              <label>{price.name}</label>
+                  <div>
+                    <span className="text-sm font-medium text-gray-700">
+                      {category?.name}
+                    </span>
+                  </div>
+                </ul>
+              ))}
             </div>
-          ))}
-          <Button
-            title={"RESET FILTERS"}
-            className="bg-color_secondary text-color_white"
-            handleClick={resetFilter}
-          />
-        </div>
-        <div>
-          <h1>All products </h1>
-          <div className="flex flex-wrap">
+            <div className="mt-2 border">
+              <h1 className="py-4">FILTER'S BY PRICE</h1>
+              {Prices.map((price) => (
+                <React.Fragment key={price._id}>
+                  <div>
+                    <input
+                      type="radio"
+                      name="price"
+                      value={price.array}
+                      id={`price-${price._id}`}
+                      className="peer hidden"
+                      checked={selectedPrice === price.array}
+                      onChange={() => handleRadioChange(price.array)}
+                    />
+                    <label
+                      htmlFor={`price-${price._id}`}
+                      className="flex justify-start cursor-pointer items-center rounded-md border border-gray-100 bg-white px-3 py-2 text-gray-700 hover:border-gray-200  peer-checked:bg-gray-300 "
+                    >
+                      <p className="text-sm font-medium">{price.name}</p>
+                    </label>
+                  </div>
+                </React.Fragment>
+              ))}
+            </div>
+          </div>
+
+          <div className="flex flex-wrap  justify-center items-center ">
             {products.map((product) => (
               <div key={product._id}>
-                <div className="max-w-sm product-card border">
-                  <a href="#" className="flex justify-center">
-                    <img
-                      className="rounded-t-lg"
-                      src={`${API_ENDPOINT}/api/v1/product/product-image/${product._id}`}
-                      alt={product.name}
-                    />
-                  </a>
-                  <div className="p-5">
-                    <a href="#">
-                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        {product.name}
-                      </h5>
-                    </a>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                      {product.description}
-                    </p>
-                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                      {product.price}
-                    </p>
+                <div className="w-72  flex  flex-col m-2  max-w-sm bg-white    shadow  border border-gray-300 rounded-xl  duration-100 hover:scale-95">
+                  <img
+                    className="p-8 rounded-t-lg object-contain h-56"
+                    src={`${API_ENDPOINT}/api/v1/product/product-image/${product._id}`}
+                    alt="product image"
+                  />
+                  <div className="px-5 pb-5">
+                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 ">
+                      {product?.name}
+                    </h5>
+                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 ">
+                      {product?.description}
+                    </h5>
+                    <div className="flex items-center mt-2.5 mb-5">
+                      <svg
+                        className="w-4 h-4 text-gray-200 dark:text-gray-600"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                      >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                      </svg>
+                      <svg
+                        className="w-4 h-4 text-gray-200 dark:text-gray-600"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                      >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                      </svg>
+                      <svg
+                        className="w-4 h-4 text-gray-200 dark:text-gray-600"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                      >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                      </svg>
+                      <svg
+                        className="w-4 h-4 text-gray-200 dark:text-gray-600"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                      >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                      </svg>
+                      <svg
+                        className="w-4 h-4 text-gray-200 dark:text-gray-600"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 22 20"
+                      >
+                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                      </svg>
+                      <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
+                        0.0
+                      </span>
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <span className="text-3xl font-bold text-gray-900">
+                        ${product?.price}
+                      </span>
+                    </div>
+                    <div className="card-actions flex gap-2 mt-1">
+                      <Button
+                        title={"View More"}
+                        className="bgGreen"
+                        handleClick={() =>
+                          navigate(`/product/${product?.slug}`)
+                        }
+                      />
+                      <Button
+                        title={"Add to cart"}
+                        className="bgYellow"
+                        handleClick={() => addToCart(product)}
+                      />
+                    </div>
                   </div>
-                  <Button
-                    title={"view more "}
-                    className="bg-color_secondary text-color_white"
-                    handleClick={() => navigate(`/product/${product.slug}`)}
-                  />
-                  <Button
-                    title={"Add to cart"}
-                    handleClick={() => addToCart(product)}
-                    className="bg-color_primary text-color_white"
-                  />
                 </div>
               </div>
             ))}
           </div>
         </div>
-      </div>
+      </main>
     </>
   );
 }
