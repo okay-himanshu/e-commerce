d9eefa08d7094c080fb08963ff3b59576db4b5ef
Himanshu <kumarhimanshu2219@gmail.com>
2023-10-31 20:20:29 +0530

    hero section redesigned

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,34 +1,52 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import Button from "./Button";
+import React, { useEffect, useState } from "react";
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
+
+import { hero1, hero2, hero3 } from "../images";
 
 function Hero() {
+  const images = [hero1, hero2, hero3];
+  const [image, setImage] = useState(hero1);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const next = () => {
+    setCurrentIndex(currentIndex + 1);
+    setImage(images[currentIndex + 1]);
+    if (currentIndex + 1 === images.length) {
+      setCurrentIndex(0);
+      setImage(images[0]);
+    }
+  };
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      next();
+    }, 6000);
+    return () => clearInterval(id);
+  }, [next]);
+
+  const prev = () => {
+    const newIndex = (currentIndex - 1 + images.length) % images.length;
+    setCurrentIndex(newIndex);
+    setImage(images[newIndex]);
+  };
+
   return (
     <>
-      {/* <section className="bg-gray-50">
-        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
-          <div className="mx-auto max-w-xl text-center">
-            <h1 className="text-3xl font-extrabold sm:text-5xl">
-              Explore Trend Tribe
-              <strong className="font-extrabold text-red-700 sm:block">
-                Your Premier Shopping Destination
-              </strong>
-            </h1>
-
-            <p className="mt-4 sm:text-xl/relaxed">
-              Elevate Your Retail Experience. Find Unique Products and Savings.
-            </p>
-
-            <div className="mt-8 flex flex-wrap justify-center gap-4">
-              <Button className="bgRed" title={"Start shopping"} />
-
-              <NavLink className="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto">
-                Learn More
-              </NavLink>
-            </div>
-          </div>
+      <div className=" relative mx-auto  max-w-screen-2xl justify-center lg:flex  lg:items-center ">
+        <img src={image} alt="" className="w-full " />
+        <div
+          className="absolute right-0 top-1/2 text-white text-2xl sm:text-6xl cursor-pointer hover:border"
+          onClick={next}
+        >
+          <IoIosArrowForward />
         </div>
-      </section> */}
+        <div
+          className="absolute left-0 top-1/2 text-white text-2xl sm:text-6xl cursor-pointer hover:border "
+          onClick={prev}
+        >
+          <IoIosArrowBack />
+        </div>
+      </div>
     </>
   );
 }
