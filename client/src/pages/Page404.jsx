001ded16d9686740981d8b946540dbb9d0729d05
Himanshu <kumarhimanshu2219@gmail.com>
2023-11-01 21:10:38 +0530

    page 404 redesigned

--- client/src/pages/Page404.jsx
+++ client/src/pages/Page404.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import { pnf } from "../svgs";
+import { Button } from "../components";
 
 function Page404() {
-  return <div>Page404</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="grid h-screen px-4 bg-white place-content-center">
+      <div className="text-center">
+        <img src={pnf} alt="page not found" />
+        <h1 className="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Uh-oh!
+        </h1>
+
+        <p className="mb-4 mt-4 text-gray-500">We can't find that page.</p>
+      </div>
+      <Button
+        handleClick={() => {
+          navigate("/");
+        }}
+        className="bgGreen"
+        title={"GO BACK HOME"}
+      />
+    </div>
+  );
 }
 
 export default Page404;
